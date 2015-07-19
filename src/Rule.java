2b33c5094c785a6c846b8a580b0bdef324ad30fa
Andi <schwoerer.andr-tit14@home.local>
2015-07-19 22:49:34 +0200

    Minor Doc change

--- src/Rule.java
+++ src/Rule.java
@@ -5,7 +5,8 @@
  */
 public abstract class Rule {
 
-	/** This method calls the applyRule of every Rule.
+	/** 
+	 * This method calls the applyRule of every Rule.
 	 * @param neighbours Array of Cells which are direct neigbours of the current Cell
 	 * @param currentCell Cell in the middle of its neighbours
 	 * @return Returns the next State of the current Cell depending on the outcome of the Rule
@@ -14,7 +15,8 @@ public abstract class Rule {
 		return applyRule(neighbours, currentCell);
 	}
 
-	/** Abstract method that is called by apply(Cell[] neighbours, Cell currentCell) to determine whether a Cell is alive in the next Generation or not. 
+	/** 
+	 * Abstract method that is called by apply(Cell[] neighbours, Cell currentCell) to determine whether a Cell is alive in the next Generation or not. 
 	 * @param neighbours Array of Cells which are direct neigbours of the current Cell
 	 * @param currentCell Cell in the middle of its neighbours
 	 * @return Returns next state of the currentCell
