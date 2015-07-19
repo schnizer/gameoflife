26e5c2cff11a89bd43069ab8689b28a7d4c65a28
boernyboerny <boernyboerny@gmail.com>
2015-07-19 15:18:14 +0200

    DOC_01

--- src/SimulationEngine.java
+++ src/SimulationEngine.java
@@ -1,18 +1,23 @@
+/**
+ * @author 2883670
+ *
+ */
 public class SimulationEngine {
 
 	static enum EdgeMode {								//New datatype EdgeMode for framemode (TORUS, BORDERED).
 		TORUS, BORDERED
 	};
 
+	
 	private final EdgeMode mode;						//Declaration of an EdgeMode called mode.
 
 	private Cell[][] cells;								//Declaration of an 2d array called cells.
 	private Rule[] rules;								//Declaration of an array called rules.
 /**
  * 
- * @param mode
- * @param rules
- * @param cells
+ * @param mode represents TORUS or BORDERED.
+ * @param rules is an array with the parameters which decide DEAD or ALIVE.
+ * @param cells is the complete array from the parser.
  */
 	public SimulationEngine(EdgeMode mode, Rule[] rules, Cell[][] cells) {
 
@@ -28,20 +33,23 @@ public class SimulationEngine {
 	}
 /**
  * 
- * @param x
- * @param y
- * @param state
+ * @param x is one of the coordinate from the Cell array.
+ * @param y is one of the coordinate from the Cell array.
+ * @param state is the current value of Alive or Dead.
  */
 	public void setCellAtTo(int x, int y, Cell.State state) {
 		this.cells[y][x].setBufferState(state);
 		this.cells[y][x].persistBufferState();
 	}
 
+	/**
+	 * reviews each cell for the next generation weather they ALIVE or DEAD.
+	 */
 	public void tick() {
 
 			Cell[] neighbours = new Cell[8];
 
-			// Set Buffer State for each cell
+				// Set Buffer State for each cell
 			for (int y = 0; y < cells.length; y++) {
 				for (int x = 0; x < cells[0].length; x++) {
 
@@ -73,9 +81,9 @@ public class SimulationEngine {
 	}
 /**
  * 
- * @param x
- * @param y
- * @return
+ * @param x is one of the coordinate from the Cell array.
+ * @param y is one of the coordinate from the Cell array.
+ * @return the position of the Cell.
  */
 	private Cell getCell(int x, int y) {
 		if (x < 0) {
