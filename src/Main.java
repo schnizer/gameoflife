205029d1c61cb08174ceccca3f882e217492527d
Andi <schwoerer.andr-tit14@home.local>
2015-07-19 17:36:56 +0200

    Errorhandling for Sleeptime and Path

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,43 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
 
 public class Main {
 
 	public static void main(String[] args) {
-
+		
+		if(args.length < 4) {
+			printErrorMessage("Invalid number of input parameters. Should be four. For example: GameOfLive /home/user/inputfile.txt 300 torus gui");
+		}
+		
 		String inputFilePath = null;
 		int sleepTime = 0;
 		SimulationEngine.EdgeMode edgeMode = null;
 		String guiMode = null;
-
-		inputFilePath = args[0];
-		sleepTime = Integer.parseInt(args[1]);
+		
+		try {
+			Paths.get(args[0]);
+			inputFilePath = args[0];
+		} catch (InvalidPathException ipe) {
+			printErrorMessage("Inputfile does not seem to be a vaild path.");
+		}
+		
+		try {
+			sleepTime = Integer.parseInt(args[1]);
+		} catch (NumberFormatException nfe) {
+			printErrorMessage("Sleeptime does not seem to be a number.");
+		}
+		
 		if (args[2].equalsIgnoreCase("torus") || args[2].equalsIgnoreCase("bordered")) {
 			edgeMode = parseEdgeMode(args[2].toLowerCase());
 		} else {
-			printErrorMessage("Wrong inputparameters");
+			printErrorMessage("Wrong inputparameters: Mode must be \"torus\" or \"bordered\"");
 		}
 		if (args[3].equalsIgnoreCase("cli") || args[3].equalsIgnoreCase("gui")) {
 			guiMode = args[3].toLowerCase();
 		} else {
-			printErrorMessage("Wrong inputparameters");
+			printErrorMessage("Wrong inputparameters: UI must be \"cli\" or \"gui\"");
 		}
 
 		Rule[] rules = { new RPopulation() };
