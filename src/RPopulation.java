6e3221bdb1c5f11797dd0d311d2bb90399dd25d5
Andi <schwoerer.andr-tit14@home.local>
2015-07-19 16:40:00 +0200

    Document RPop & Rule

--- src/RPopulation.java
+++ src/RPopulation.java
@@ -1,24 +1,28 @@
 import java.util.Arrays;
 
+/**
+ * @author Andi
+ *
+ */
 public class RPopulation extends Rule {
 
 	@Override
 	protected Cell.State applyRule(Cell[] neighbours, Cell currentCell) {
 
-		long liveCellCount = Arrays.stream(neighbours)
-				.filter(c -> (c.getState() == Cell.State.ALIVE)).count();
+		long livingCellCount = Arrays.stream(neighbours)
+				.filter(c -> (c.getState() == Cell.State.ALIVE)).count(); // Convert array to stream, filter out everything that isn't alive and count the occurrences.
 
-		if (currentCell.getState() == Cell.State.ALIVE) {
-			if (liveCellCount < 2 || liveCellCount > 3) {
-				return Cell.State.DEAD;
+		if (currentCell.getState() == Cell.State.ALIVE) { // Action for cells that are alive
+			if (livingCellCount < 2 || livingCellCount > 3) { 
+				return Cell.State.DEAD;  // Kill if over- or underpopulated
 			} else {
-				return Cell.State.ALIVE;
+				return Cell.State.ALIVE; // Stay alive
 			}
-		} else {
-			if (liveCellCount == 3) {
-				return Cell.State.ALIVE;
+		} else { 										 // Action for cells that are dead
+			if (livingCellCount == 3) {
+				return Cell.State.ALIVE; // A new cell is born
 			} else {
-				return Cell.State.DEAD;
+				return Cell.State.DEAD;  // Cell stays dead
 			}
 		}
 
--- src/Rule.java
+++ src/Rule.java
@@ -4,7 +4,7 @@
  */
 public abstract class Rule {
 
-	/**
+	/** This method calls the applyRule of every Rule.
 	 * @param neighbours Array of Cells which are direct neigbours of the current Cell
 	 * @param currentCell Cell in the middle of its neighbours
 	 * @return Returns the next State of the current Cell depending on the outcome of the Rule
@@ -16,7 +16,7 @@ public abstract class Rule {
 	/** Abstract method that is called by apply(Cell[] neighbours, Cell currentCell) to determine whether a Cell is alive in the next Generation or not. 
 	 * @param neighbours Array of Cells which are direct neigbours of the current Cell
 	 * @param currentCell Cell in the middle of its neighbours
-	 * @return
+	 * @return Returns next state of the currentCell
 	 */
 	protected abstract Cell.State applyRule(Cell[] neighbours,
 			Cell currentCell);
